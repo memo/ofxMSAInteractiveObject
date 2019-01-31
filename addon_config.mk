cedc8716151d4b4b34acb20e092351c6fa94c292
Memo Akten <memo@memo.tv>
2019-02-01 02:19:09 +0300

    added addon_config and updated example

---
+++ addon_config.mk
@@ -0,0 +1,6 @@
+meta:
+	ADDON_NAME = ofxMSAInteractiveObject
+	ADDON_DESCRIPTION = C++ openFrameworks addon which wraps up some auto event functionality
+	ADDON_AUTHOR = Memo Akten, www.memo.tv
+	ADDON_TAGS = "GUI"
+	ADDON_URL = https://github.com/memo/ofxMSAInteractiveObject
